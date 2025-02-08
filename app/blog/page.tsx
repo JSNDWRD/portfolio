0cc707e09ec5b73754f578279883543742109dce
JSNDWRD <jasonedwardsalim@gmail.com>
2025-02-08 10:51:25 +0700

    UI fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,7 @@
-import Image from "next/image";
-import background from "@/public/card.png";
-
 export default function page() {
   return (
-    <div className="flex h-[100dvh] justify-center">
-      <Image
-        src={background}
-        alt="background"
-        className="h-[100dvh] max-h-[100dvh] object-cover object-center"
-      />
-      <h1 className="absolute top-1/2 -translate-y-1/2 self-center">
-        Developing...
-      </h1>
+    <div className="flex h-[100dvh] justify-center bg-card bg-cover bg-center">
+      <h1 className="self-center">Developing...</h1>
     </div>
   );
 }
