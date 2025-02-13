58f2907a749793e2b3c7f46dd15d0c4befe5858e
JSNDWRD <jasonedwardsalim@gmail.com>
2025-02-13 23:21:32 +0700

    UI fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 export default function page() {
   return (
-    <div className="flex max-h-[100dvh] justify-center bg-card bg-cover bg-center">
+    <div className="flex h-[100dvh] justify-center bg-card bg-cover bg-center">
       <h1 className="self-center">Developing...</h1>
     </div>
   );
